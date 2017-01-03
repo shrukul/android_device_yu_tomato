157a3986dd4f5151a7549a12383142b54d249d25
shrukul <shrukul99@gmail.com>
2017-01-03 12:36:43 +0530

    Rebrand to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := cm_tomato
+PRODUCT_NAME := lineage_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
